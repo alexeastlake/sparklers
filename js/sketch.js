76b86604f00105270239dacd1be458afa025940e
Alex Eastlake <alex.eastlake@gmail.com>
2024-03-16 14:33:09 +1300

    Initial scaffold commit

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+const FRAMERATE = 60;
+
+function setup() { 
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  frameRate(FRAMERATE);
+}
+
+function draw() {
+  background(0);
+}
